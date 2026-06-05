9526e5cdd1278d44fa7d50b9908f9224c32d8c53
sayem hasan <imsayem678@gmail.com>
2026-06-05 16:52:40 +0600

    Fast Sum

--- LabAssignment1/Problem3.java
+++ LabAssignment1/Problem3.java
@@ -1,2 +1,18 @@
-// LabAssignment1 - Problem3
-// Replace this placeholder with your Codeforces solution.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+ 
+public class no3Clab1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true); // auto flush
+ 
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            long n = Long.parseLong(br.readLine());
+            long sum = (n * (n + 1)) / 2;
+            pw.println(sum);
+        }
+    }
+}
