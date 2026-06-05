8b41d29f8bd9851df51c5f72d410606a29a99794
sayem hasan <imsayem678@gmail.com>
2026-06-05 16:53:22 +0600

    Implement solution for Problem 4 in Lab Assignment 1
    Is Sorted?

--- LabAssignment1/Problem4.java
+++ LabAssignment1/Problem4.java
@@ -1,2 +1,35 @@
-// LabAssignment1 - Problem4
-// Replace this placeholder with your Codeforces solution.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class no4Dlab1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true); 
+        //boolean flag=false;
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int arr[] = new int [n];
+            boolean flag = false; 
+
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int x=Integer.parseInt(st.nextToken());
+                arr[j]=x;
+            }
+            for (int k = 0; k < arr.length-1; k++) {
+                if (arr[k+1] < arr[k]){
+                    flag=true;
+                    //pw.println("NO");
+                    //return;
+                }
+            }
+            if (flag==true){pw.println("NO");}
+            else{pw.println("YES");}
+        }
+    }
+}
