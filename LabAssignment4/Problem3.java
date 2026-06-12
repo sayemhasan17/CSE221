4e658ab249d1ac01d87e2598dcca8ea2c189fa64
sayem hasan <imsayem678@gmail.com>
2026-06-12 23:10:42 +0600

    Add solution for Problem 3 in TaskC class
    Implemented a solution for Problem 3 using a matrix to store input data.

--- LabAssignment4/Problem3.java
+++ LabAssignment4/Problem3.java
@@ -1,2 +1,29 @@
-// LabAssignment4 - Problem3
-// Replace this placeholder with your Codeforces solution.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TaskC {
+    public static void main(String[]args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw=new PrintWriter(System.out);
+        int n=Integer.parseInt(br.readLine());
+        int[][]mat=new int[n][n];
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            int c=Integer.parseInt(st.nextToken());
+            for(int j=0; j<c; j++){
+                mat[i][Integer.parseInt(st.nextToken())]=1;
+            }
+        }
+         for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+        pw.flush();
+    }
+    
+}
