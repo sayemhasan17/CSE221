b282af57324e0c4419ef11e092ce5db7cab1fbd2
sayem hasan <imsayem678@gmail.com>
2026-06-12 23:09:37 +0600

    Implement graph adjacency matrix in Problem1.java
    Added a Java program to read a graph's edges and display its adjacency matrix.

--- LabAssignment4/Problem1.java
+++ LabAssignment4/Problem1.java
@@ -1,2 +1,34 @@
-// LabAssignment4 - Problem1
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class TaskA {
+    public static void main(String[]args) throws Exception{
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw=new PrintWriter(System.out);
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int v=Integer.parseInt(st.nextToken());
+        int e=Integer.parseInt(st.nextToken());
+        int[][]mat=new int[v][v];
+
+        for(int i=0; i<e; i++){
+            st=new StringTokenizer(br.readLine());
+            int ui=Integer.parseInt(st.nextToken());
+            int vi=Integer.parseInt(st.nextToken());
+            int wi=Integer.parseInt(st.nextToken());
+            mat[ui-1][vi-1]=wi;
+        }
+
+        for(int i=0; i<v; i++){
+            for(int j=0; j<v; j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+        pw.flush();
+    }
+    
+}// LabAssignment4 - Problem1
 // Replace this placeholder with your Codeforces solution.
